3c8acf62d21c448614744649665b8a669d944880
tiffy74 <tiffany.prosser1@gmail.com>
2019-07-24 01:23:16 +0100

    Update SmallestIntegerFinder.js

--- SmallestIntegerFinder.js
+++ SmallestIntegerFinder.js
@@ -1,7 +1,6 @@
 class SmallestIntegerFinder {
   findSmallestInt(args) {
-    var arrange = args.sort(
-    function(a, b){return a - b});
-    return arrange[0];
+    args.sort(function(a, b){return a - b});
+    return args[0];
   }
 }
